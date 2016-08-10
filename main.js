0ac5abc10e515b65e1027620ba43989ec7936539
Gina <gdebell@gmail.com>
2016-08-10 08:46:00 -0600

    Nutri hits API and returns a list of data according to user serach.  When user selects a specific food item from the search list, the food item, calories, and total fat displays into a table.  The total calories and total fat is able to calulate (in the console- not yet displayed).  Able to append information at the bottom of the table.  (It appends each time- needs to replace OR append only once.)  Bugs that still need to get worked out: will not display first item in select bar if that is the item that is selected.

--- main.js
+++ main.js
@@ -2,44 +2,104 @@ $(document).on('ready', function(e) {
   console.log("Sanity Check!");
 })
 
+var calorieTotal = 0;
+var fatTotal = 0;
 
 $(function() {
   $('form').on('submit', function(e) {
     e.preventDefault();
     var dataFromSite = ($('#fruitInput').val());
+    $('#fruitInput').val('');
 
   $.ajax ({
     method: 'GET',
     url:'https://api.nutritionix.com/v1_1/search/' + dataFromSite + '?fields=item_name%2Cnf_calories%2Cnf_total_fat&appId=530e689d&appKey=cdb9516e92068d4107bdfbb609f61010',
 
   }).done (function(data) {
-    var c = data.hits
-    for (key in c) {
-      console.log(c[key]);
-      console.log('Item Name: ' + c[key].fields.item_name);
-      //console.log('Calories: ' + c[key].fields.nf_calories);
-      var foodCalories = c[key].fields.nf_calories;
-      console.log(foodCalories);
-      //console.log('Total fat: ' + c[key].fields.nf_total_fat);
-      var foodFat = c[key].fields.nf_total_fat;
-      console.log(foodFat);
-      console.log('                          ');
+      var c = data.hits
+      var listOfHits = [];
 
-      var searchOptions = c[key].fields.item_name;
+      for (key in c) {
+        //console.log(c[key]);
+        listOfHits.push(c[key]);
+      };
 
-      $('#output').append('<option>' + searchOptions + '</option>');
+      var foods = [];
+      //console.log(listOfHits);
+      listOfHits.forEach(function(object) {
+        var itemName = object.fields.item_name;
+        foods.push({
+          name: itemName,
+          calories: object.fields.nf_calories,
+          fat: object.fields.nf_total_fat
+        });
+      });
+
+      console.log(foods);
+      //console.log(foods[0].name);
+
+      for (var i = 0; i< foods.length; i++) {
+        $('#output').append('<option>' + foods[i].name + '</option>');
+      }
+
 
 
       $('#output').change(function(e) {
         var foodItemSelected = $('#output').val();
-        console.log(c[key].fields.nf_calories);
-        console.log('Food Item Selected: ' + foodItemSelected);
-        //console.log('Info of selected item ' + )
+
+
+        for (var i = 0; i< foods.length; i++) {
+            //refactor with filter at a later time
+          if (foods[i].name === foodItemSelected) {
+            var matchPosition = i;
+            $('#resultTable').append('<tr><td>' + foods[matchPosition].name + '</td><td>' + foods[matchPosition].calories + '</td><td>' + foods[matchPosition].fat + '</td></tr>');
+            //console.log("CALORIE INFO:  ");
+            //console.log(foods[matchPosition].calories);
+            //console.log(typeof foods[matchPosition].calories);
+            console.log("FAT INFORMATION: ");
+            console.log(foods[matchPosition].fat);
+            console.log(typeof foods[matchPosition].fat);
+
+            calorieTotal += (foods[matchPosition].calories);
+
+            fatTotal += (foods[matchPosition].fat);
+
+            //console.log("CAL TOTAL>>>>>>>>");
+            //console.log(calorieTotal);
+            console.log("FAT TOTAL::::::::");
+            console.log(fatTotal);
+
+            $('#resultTable:last').append('<p>Hi</p>');
+
+
+            break;
+          }
+
+
+        }
 
 
 
-      $('#results').append('<p>' + foodItemSelected + '</p>');
+          })
+        });
       });
+    });
+
+
+
+
+
+
+
+
+
+      //   console.log('Food Item Selected: ' + foodItemSelected);
+      //   //console.log('Info of selected item ' + )
+      //
+      //
+      //
+      // $('#results').append('<p>' + foodItemSelected + '</p>');
+      // });
 
 
       // $('#select2').change(function(e) {
@@ -50,8 +110,8 @@ $(function() {
 
 
 
-
-    }; }); }); });
+    //
+    // }; }); }); });
 
 
 
@@ -65,20 +125,3 @@ $(function() {
 //
 //   });
 // });
-
-
-
-
-
-
-
-
-// https://api.nutritionix.com/v1_1/search/mcdonalds?results=0:20&fields=item_name,brand_name,item_id,nf_calories&appId=530e689&appKey=cdb9516e92068d4107bdfbb609f61010
-
- // https://api.nutritionix.com/v1_1/search/celery&ppId=530e689d&appKey=cdb9516e92068d4107bdfbb609f61010
-// // v1_1/search/{phrase}
-//
-// https://api.nutritionix.com/v1_1/search/celery?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat&appId=530e689d&appKey=cdb9516e92068d4107bdfbb609f61010
-
-// https://api.nutritionix.com/v1_1/search/celery?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat&appId=530e689d&appKey=cdb9516e92068d4107bdfbb609f61010
-// https://api.nutritionix.com/v1_1/search/celery?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat&appId=530e689d&appKey=cdb9516e92068d4107bdfbb609f61010
